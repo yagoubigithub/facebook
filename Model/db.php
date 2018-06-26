bc3da963a22b7aa70f092f1191d9762cf90c9f83
Yagoubi Aek <yagoubi.aek.2@gmail.com>
2018-06-26 03:21:30 +0100

    change variable  to

--- Model/db.php
+++ Model/db.php
@@ -5,20 +5,20 @@ $tf_username = 'root';
 $tf_password = '';
 
 // Create connection
-$tf_handle = mysqli_connect($tf_host, $tf_username, $tf_password,$tf_dbname);
+$facebook_handle = mysqli_connect($tf_host, $tf_username, $tf_password,$tf_dbname);
 
 // Check connection
-if (!$tf_handle) {
+if (!$facebook_handle) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
 //
-mysqli_query($tf_handle,"SET NAMES 'utf8'");
+mysqli_query($facebook_handle,"SET NAMES 'utf8'");
 
 // close connection
 function tinyf_db_close(){
-    global $tf_handle;
-    mysqli_close($tf_handle);
+    global $facebook_handle;
+    mysqli_close($facebook_handle);
 }
 
 ?>
