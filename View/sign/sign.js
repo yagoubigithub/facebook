73295802d228b951a62685c10b0f0e44cc6e3394
Yagoubi Aek <yagoubi.aek.2@gmail.com>
2018-06-28 02:53:39 +0100

    configure sign.js to send email and password to login.php

---
+++ View/sign/sign.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("#submit_login").click(function () {
+        var email = $("#email_login").val();
+        var password = $("#password_login").val();
+        $.post('../../controller/login.php', {
+                email: email,
+                password: password
+            },
+            function (data) {
+
+            })
+    })
+});
