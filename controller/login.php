b51955c7d80a59566e2dd81042c2b173aa5f2a96
Yagoubi Aek <yagoubi.aek.2@gmail.com>
2018-07-12 00:17:17 +0100

    creaate  object

--- controller/login.php
+++ controller/login.php
@@ -5,8 +5,16 @@ require_once('../Model/usersAPI.php');
 if (isset($_GET['email']) && isset($_GET['password'])) {
     $user = usersAPI_f_select_user_by_email_and_password($_GET['email'], $_GET['password']);
     if ($user != null) {
-        
-        
+        // create session to use in other pages
+        $_SESSION['id'] = $user['id'];
+        $_SESSION['firstname'] = $user['firstname'];
+        $_SESSION['lastname'] = $user['lastname'];
+        $_SESSION['email'] = $user['email'];
+        $_SESSION['sex'] = $user['sex'];
+        $_SESSION['birthday'] = $user['birthday'];
+        $_SESSION['image_profile'] = $user['url'];
+        $_SESSION['isadmin'] = $user['isadmin'];
+
         //set email cookie
         $cookie_email_name = sha1('email_facebook');
         $cookie_email_value = $user['email'];
