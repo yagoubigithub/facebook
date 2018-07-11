9a0c3178139abae8327702ac3aee824491fed47a
Yagoubi Aek <yagoubi.aek.2@gmail.com>
2018-07-11 23:00:02 +0100

    update user json object

--- controller/user.php
+++ controller/user.php
@@ -7,16 +7,9 @@ if (isset($_GET['id'])) {
     $uid = $_GET['id'];
     $user = usersAPI_f_select_user_by_id($uid);
     if($user != null){
-        echo '{';
-            echo "\"id\" : " .$uid . ",";
-            echo "\"firstname\" : \"" . $user['firstname'] . "\",";
-            echo "\"image_profile\" : \"" . $user['url'] . "\",";
-            echo "\"lastname\" : \"" . $user['lastname'] . "\"";
-            echo '}';
+      echo json_encode($user);
     }else{
         echo 'null';
     }
-    
-
 }
 ?>
