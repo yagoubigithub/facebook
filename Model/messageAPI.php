2b586dda4c5efbb0e569d9548a0d599101680e28
Yagoubi Aek <yagoubi.aek.2@gmail.com>
2018-08-04 16:12:21 +0100

    messageAPI_select_all_messages

--- Model/messageAPI.php
+++ Model/messageAPI.php
@@ -35,5 +35,30 @@ function messageAPI_insert_Message($mesg,$sender_id,$receiver_id){
    
 
 }
+function messageAPI_select_all_messages($sender_id,$receiver_id){
+    try{
+        global $facebook_handle;
+        $n_sender_id = (int)$sender_id;
+        $n_receiver_id = (int)$receiver_id;
 
+        
+    
+        $query = sprintf("SELECT *  FROM `message` WHERE sender_id = %d AND receiver_id = %d", $n_sender_id,$n_receiver_id);
+        $query_result = mysqli_query($facebook_handle, $query);
+    
+        if (!$query_result)
+            return null;
+    
+        $message = null;
+        $message = mysqli_fetch_assoc($query_result);
+    
+    
+        return $message;
+    
+    }catch(Exepetion $ex){
+        error_log($ex->getMessage());
+        return false;
+    }
+   
+}
 ?>
