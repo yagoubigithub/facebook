b6327573c7488445453d12ff0cdc47d217d7bc41
Yagoubi Aek <yagoubi.aek.2@gmail.com>
2018-08-07 06:25:45 +0100

    update insert_Message controller

--- controller/insert_Message.php
+++ controller/insert_Message.php
@@ -7,9 +7,16 @@ if (isset($_POST['mesg']) && isset($_POST['sender_id']) && isset($_POST['receive
     $sender_id = $_GET['sender_id'];
     $receiver_id = $_GET['receiver_id'];
 
-    $messages = messageAPI_insert_Message($mesg,$sender_id,$receiver_id);
-    if ($messages){
-        echo "true";
+    $is_messages = messageAPI_insert_Message($mesg,$sender_id,$receiver_id);
+    if ($is_messages){
+        $messages = messageAPI_select_all_messages($sender_id,$receiver_id);
+        if ($messages == null)
+            echo 'null';
+        else{
+            echo json_encode($messages);
+        }
+    }else{
+        echo 'null';
     }
       
 
