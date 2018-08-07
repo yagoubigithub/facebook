850e07cce20326f0687717620a4edd1690e0b2e6
Yagoubi Aek <yagoubi.aek.2@gmail.com>
2018-08-07 09:16:44 +0100

    messageAPI_delete_the_new_Message  function

--- Model/messageAPI.php
+++ Model/messageAPI.php
@@ -70,6 +70,37 @@ function messageAPI_insert_new_Message($mesg,$sender_id,$receiver_id){
     }
    
 }
+/******delete the  new message into new_message table in database */
+function messageAPI_delete_the_new_Message($sender_id,$receiver_id){
+    try{
+        global $facebook_handle;
+        if (empty($sender_id) || empty($receiver_id)) {
+            error_log("sender_id or receiver_id is empty()");
+            return false;
+        }
+       
+        $n_sender_id = (int)$sender_id;
+        $n_receiver_id = (int)$receiver_id;
+    
+    
+        $query = sprintf(
+            "DELETE FROM `new_message` WHERE `sender_id`=%d  AND `receiver_id`=%d",
+            $n_sender_id,
+            $n_receiver_id
+        );
+    
+        $query_result = mysqli_query($facebook_handle, $query);
+        if (!$query_result)
+            return false;
+        return true;
+    
+    }catch(Exepetion $ex){
+        error_log($ex->getMessage());
+        return false;
+    }
+   
+}
+
 function messageAPI_select_all_messages($sender_id,$receiver_id){
     try{
         global $facebook_handle;
