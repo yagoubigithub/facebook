cc25c2ad885435ee0bce7b458980919446ef7426
Yagoubi Aek <yagoubi.aek.2@gmail.com>
2018-08-13 06:24:32 +0100

    get method to get number of new message

--- View/home-nav-bar/home_navbar.js
+++ View/home-nav-bar/home_navbar.js
@@ -5,6 +5,20 @@ $(document).ready(function(){
 
         } else {
             uid = Number(data);
+
+              
+            $.get('./controller/select_number_new_message.php', {
+               
+                receiver_id: uid
+            },
+            function (data) {
+                if (data != 'null') {
+                    var number_of_message = JSON.parse(data);
+
+                }
+                
+
+            });
         }
         });
 });
