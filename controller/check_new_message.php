3353488f572b368681db98bd9ab0b68035199407
Yagoubi Aek <yagoubi.aek.2@gmail.com>
2018-08-14 07:04:26 +0100

    chech_new message and print number of message

--- controller/check_new_message.php
+++ controller/check_new_message.php
@@ -8,9 +8,10 @@ if (isset($_GET['sender_id']) && isset($_GET['receiver_id'])) {
     $receiver_id = $_GET['receiver_id'];
     $is_new_messages_exist = messageAPI_if_is_a_new_message($sender_id, $receiver_id);
     if ($is_new_messages_exist) {
-       
-    }else{
-        
+        $nb_mesg = messageAPI_select_number_of_new_messages_bysendrId_receiverId($sender_id, $receiver_id);
+        echo $nb_mesg;
+    } else {
+        echo "0";
     }
 
 }
